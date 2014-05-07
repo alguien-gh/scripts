f958118af0b0868aa6b9c29a7b5d5935c814cb93
alguien-gh <csar.1603@gmail.com>
2014-05-07 08:55:57 -0500

    agregar OUI F86394 y resolver incompatibilidad con python3

--- wifi/wlanXXXX.py
+++ wifi/wlanXXXX.py
@@ -12,6 +12,8 @@ Historial de cambios:
 05/04/2014    Agregar OUI F86180 reportado por Miguel Mallma
 07/04/2014    Agregar OUI F87F35 reportado por @francis2077
 29/04/2014    Soporte para OUI F8C346 de la marca Observa
+07/05/2014    Agregar OUI F86394 marca Observa (aporte anonimo)
+07/05/2014    Resuelta incompatibilidad con python3 (reportado por jon4)
 """
 
 import sys
@@ -27,7 +29,8 @@ zteOUI = {
 }
 
 observaOUI = {
-    'F8C346': 'E4C146'    # Aporte anonimo
+    'F8C346': 'E4C146',   # Aporte anonimo
+    'F86394': 'D86194'    # Aporte anonimo
 }
 
 
@@ -41,15 +44,15 @@ def get_prefix(oui=""):
 
 def main():
     if len(sys.argv) != 2:
-        print "Uso: %s <BSSID>" % (sys.argv[0])
-        print "Ej.: %s 1A:2B:3C:4D:5E:6F" % (sys.argv[0])
+        print("Uso: %s <BSSID>" % (sys.argv[0]))
+        print("Ej.: %s 1A:2B:3C:4D:5E:6F" % (sys.argv[0]))
         exit(1)
 
     mac = sys.argv[1].upper()
     partes = mac.split(":")
 
     if len(partes) != 6 or len(mac) != 17:
-        print "Esto no es una MAC valida: %s" % mac
+        print("Esto no es una MAC valida: %s" % mac)
         exit(1)
 
     oui = "".join(partes)
@@ -57,15 +60,15 @@ def main():
 
     prefix = get_prefix(oui)
     if prefix is None:
-        print "Lo siento, no conosco ese OUI: %s" % oui
+        print("Lo siento, no conosco ese OUI: %s" % oui)
         exit(1)
 
     final = hex(int("".join(partes[3:]), 16) - 9).upper()[2:]
     final = "0" * (6 - len(final)) + final
 
     defpwd = "%s%s" % (prefix, final)
-    print "La clave WPA por defecto es: %s" % defpwd
-    print "Terminado };]"
+    print("La clave WPA por defecto es: %s" % defpwd)
+    print("Terminado };]")
 
 
 if __name__ == "__main__":
